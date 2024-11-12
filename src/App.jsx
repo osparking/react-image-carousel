3a9fe5a01f301b72afceacbda479f7af003a505b
osparking <jbpark03@gmail.com>
2024-11-12 21:21:59 +0900

    pass image data to Carousel compo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Carousel } from "./components/Carousel";
 
+import { slides } from "./data/carouselData.json";
+
 function App() {
   return (
     <div className="App">
-      <Carousel />
+      <Carousel data={slides} />
     </div>
   );
 }
