898096c19d73fb1c40d81ce27e487ad4bb478068
osparking <jbpark03@gmail.com>
2024-11-12 22:02:40 +0900

    arrow circle fill added for both direction

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,14 +1,17 @@
 import React from "react";
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
 import "./Carousel.css";
 
 export const Carousel = ({ data }) => {
   return (
     <div className="carousel">
+      <BsArrowLeftCircleFill />
       {data.map((item, idx) => {
         return (
           <img src={item.src} alt={item.alt} key={idx} className="slide" />
         );
       })}
+      <BsArrowRightCircleFill />
     </div>
   );
 };
