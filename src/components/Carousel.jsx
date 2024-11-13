05f4d2a133e436540e57f2da6b645edf36330134
osparking <jbpark03@gmail.com>
2024-11-13 10:10:25 +0900

    add slide state to Carousel compo'

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
 import "./Carousel.css";
 
 export const Carousel = ({ data }) => {
+  const [slide, setSlide] = useState(0);
   return (
     <div className="carousel">
       <BsArrowLeftCircleFill className="arrow arrow-left" />
       {data.map((item, idx) => {
         return (
-          <img src={item.src} alt={item.alt} key={idx} className="slide" />
+          <img
+            src={item.src}
+            alt={item.alt}
+            key={idx}
+            className={slide === idx ? "slide" : "slide slide-hidden"}
+          />
         );
       })}
       <BsArrowRightCircleFill className="arrow arrow-right" />
